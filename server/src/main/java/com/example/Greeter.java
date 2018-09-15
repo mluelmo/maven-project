9879fa1069bcc6a7cc90e9b4034d0aa812b5d8c6
Miguel <mluelmo@gmail.com>
2018-09-15 11:46:47 +0200

    Checkstyle solutions

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,8 +15,8 @@ public class Greeter {
   /**
    * 	Greet method.
    * 
-   * @param someone
-   * @return
+   * @param someone - String
+   * @return String - greet
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
